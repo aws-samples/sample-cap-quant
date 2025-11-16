7e4b0ddb07e056bf4fc855fa3ad6cb455ab61dee
symeta <97269758+symeta@users.noreply.github.com>
2025-11-16 14:13:03 +0800

    Update outputs.tf

--- quant-research/vit_tr_ray_on_gpu/infra/outputs.tf
+++ quant-research/vit_tr_ray_on_gpu/infra/outputs.tf
@@ -7,3 +7,32 @@ output "elastic_cache_redis_cluster_arn" {
   description = "Cluster arn of the cache cluster"
   value       = module.elasticache.cluster_arn
 }
+
+output "elastic_cache_redis_endpoint" {
+  description = "Cluster endpoint of the cache cluster"
+  value       = module.elasticache.cluster_cache_nodes[0].address
+}
+
+output "accesskey" {
+  value = var.accesskey
+}
+
+output "secrectkey" {
+  value = var.secrectkey
+}
+
+output "raw_data_s3bucket_https_endpoint_url" {
+  value = var.raw_data_s3bucket_https_endpoint_url
+}
+
+output "region_id" {
+  value = var.region
+}
+
+output "ray_cluster_result_s3bucket_url" {
+  value = var.ray_cluster_result_s3bucket_url
+}
+
+output "ecr_url" {
+  value = var.ecr_url
+}
