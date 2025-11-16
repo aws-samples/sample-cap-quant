3d05dbe47d403a6e15a2411c26f060ac838d9fef
symeta <97269758+symeta@users.noreply.github.com>
2025-11-16 14:09:16 +0800

    Update 01_deploy_ray_cluster.sh

--- quant-research/vit_tr_ray_on_gpu/app/01_deploy_ray_cluster.sh
+++ quant-research/vit_tr_ray_on_gpu/app/01_deploy_ray_cluster.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# 从Terraform输出获取ECR URL
 export ECR_URL=$(cd ../infra && terraform output -raw ecr_url)
-
-# 使用envsubst替换模板变量并部署
 envsubst < raycluster-with-jfs.yaml | kubectl apply -f -
