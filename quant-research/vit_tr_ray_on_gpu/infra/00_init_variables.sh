079f133b0e3cc31e6a7f13d722f227e229757916
symeta <97269758+symeta@users.noreply.github.com>
2025-11-25 20:38:52 +0800

    Update 00_init_variables.sh

--- quant-research/vit_tr_ray_on_gpu/infra/00_init_variables.sh
+++ quant-research/vit_tr_ray_on_gpu/infra/00_init_variables.sh
@@ -16,7 +16,7 @@ export TF_VAR_raw_data_s3bucket_https_endpoint_url="https://s3.us-west-2.amazona
 export TF_VAR_ray_cluster_result_s3bucket_url="s3://${TF_VAR_s3_bucket_name2}/${TF_VAR_prefix_name}"
 
 # ECR repository URL
-export TF_VAR_ecr_url="${TF_VAR_aws_account_id}.dkr.ecr.us-west-2.amazonaws.com/kuberay_cnn_gpu:V0.3"
+export TF_VAR_ecr_url="${TF_VAR_aws_account_id}.dkr.ecr.us-west-2.amazonaws.com/kuberay_cnn_gpu:latest"
 
 # Service account for pod identity
 export TF_VAR_service_account_name="ray-service-account"
