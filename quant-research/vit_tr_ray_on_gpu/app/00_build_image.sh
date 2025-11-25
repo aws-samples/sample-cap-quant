747f372378e53986370b9f048edfcf9253f4d289
symeta <97269758+symeta@users.noreply.github.com>
2025-11-25 18:55:42 +0800

    Update 00_build_image.sh

--- quant-research/vit_tr_ray_on_gpu/app/00_build_image.sh
+++ quant-research/vit_tr_ray_on_gpu/app/00_build_image.sh
@@ -67,10 +67,15 @@ echo -e "\nLogging in to ECR Public Registry"
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
 
 # Create and use a new builder instance for multi-arch builds
-docker buildx create --use --name mybuilder --driver docker-container
-docker buildx inspect mybuilder --bootstrap
+#docker buildx create --use --name mybuilder --driver docker-container
+#docker buildx inspect mybuilder --bootstrap
 
-echo -e "\nBuilding kuberay_trn1 docker image" \
-  && docker buildx build --platform linux/amd64 -t $ECR_REPO_URI:$ecr_version --build-arg REGION=$region . --push \
+#echo -e "\nBuilding kuberay_trn1 docker image" \
+#  && docker buildx build --platform linux/amd64 -t $ECR_REPO_URI:$ecr_version --build-arg REGION=$region . --push \
+#  && echo -e "\nImage successfully pushed to ECR"
+
+echo -e "\nBuilding kuberay_gpu docker image" \
+  && docker buildx build --platform linux/amd64 -t $ECR_REPO_URI:$ecr_version --build-arg REGION=$region .  \
+  && docker push $ECR_REPO_URI:$ecr_version \
   && echo -e "\nImage successfully pushed to ECR"
 
