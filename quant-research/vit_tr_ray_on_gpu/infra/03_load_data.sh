07142f56a711a77da0e97429e730c1de8687a15a
symeta <97269758+symeta@users.noreply.github.com>
2025-12-19 21:09:35 +0800

    Update 03_load_data.sh

--- quant-research/vit_tr_ray_on_gpu/infra/03_load_data.sh
+++ quant-research/vit_tr_ray_on_gpu/infra/03_load_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 kubectl apply -f data-load-pod.yaml
-kubectl wait --for=condition=Ready pod/data-load-pod --timeout=120s
+kubectl wait --for=condition=Ready pod/data-load-pod --timeout=160s
+echo "please wait for data loading..."
 
 if kubectl exec data-load-pod -- ls /data/cifar-10-python.tar.gz &>/dev/null; then
     echo "data loading success"
