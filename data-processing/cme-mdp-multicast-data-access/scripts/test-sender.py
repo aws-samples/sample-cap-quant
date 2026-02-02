705a5f9fe2549b6b60f6dae5eef852216ac77b68
symeta <97269758+symeta@users.noreply.github.com>
2026-02-02 11:42:43 +0800

    Update test-sender.py

--- data-processing/cme-mdp-multicast-data-access/scripts/test-sender.py
+++ data-processing/cme-mdp-multicast-data-access/scripts/test-sender.py
@@ -1,1 +1,15 @@
+import socket
+import time
 
+MCAST_GRP = '232.0.0.1'
+MCAST_PORT = 5000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, b'gre1')
+
+for i in range(10):
+    msg = f'Multicast test message {i}'
+    sock.sendto(msg.encode(), (MCAST_GRP, MCAST_PORT))
+    print(f'Sent: {msg}')
+    time.sleep(0.5)
